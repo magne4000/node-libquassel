09fa72e36116bee4fb4d458921f68d746c9f2e63
Joël Charles <joel.charles91@gmail.com>
2014-04-29 17:27:22 +0200

    Buffers progress

--- test/manual.js
+++ test/manual.js
@@ -1,2 +1,11 @@
 var Quassel = require('../lib/libquassel.js');
-new Quassel("getonmyhor.se", 4242);
+var quassel = new Quassel("getonmyhor.se", 4242, function() {
+    var n = quassel.getNetworks(), k, l;
+    for (k in n) {
+        console.log("Network : " + n[k].networkName)
+        var buffers = n[k].getBuffers();
+        for (l in buffers) {
+            console.log("   "+buffers[l].name);
+        }
+    }
+});
