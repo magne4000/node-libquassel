3e59f80cff18235135f8edd841ce17cfdd74fa5e
Joël Charles <joel.charles91@gmail.com>
2014-05-08 00:16:06 +0200

    Receiving backlog

--- test/manual.js
+++ test/manual.js
@@ -2,7 +2,7 @@ var Quassel = require('../lib/libquassel.js');
 var quassel = new Quassel("getonmyhor.se", 4242, function() {
     var n = quassel.getNetworks(), k, l;
     for (k in n) {
-        console.log("Network : " + n[k].networkName)
+        console.log("Network : " + n[k].networkName);
         var buffers = n[k].getBuffers();
         for (l in buffers) {
             console.log("   "+(buffers[l].name||buffers[l].id));
