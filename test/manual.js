90433270dc43c1c07aa8492cd9e6599b937f1ea8
Joël Charles <joel.charles91@gmail.com>
2014-05-14 17:13:38 +0200

    Messages fetched and affected to buffers

--- test/manual.js
+++ test/manual.js
@@ -1,11 +1,17 @@
 var Quassel = require('../lib/libquassel.js');
-var quassel = new Quassel("getonmyhor.se", 4242, function() {
-    var n = quassel.getNetworks(), k, l;
+var quassel = new Quassel("getonmyhor.se", 4242);
+
+quassel.on('BacklogManager.receiveBacklog', function() {
+    var n = quassel.getNetworks(), k, l, m;
     for (k in n) {
         console.log("Network : " + n[k].networkName);
         var buffers = n[k].getBuffers();
         for (l in buffers) {
             console.log("   "+(buffers[l].name||buffers[l].id));
+            for (m in buffers[l].messages) {
+                console.log("      " + buffers[l].messages[m].datetime + " - "
+                    + buffers[l].messages[m].content);
+            }
         }
     }
 });
