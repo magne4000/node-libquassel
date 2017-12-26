8966fb898f2bf794476f565a81e750d886249cfc
Joël Charles <joel.charles91@gmail.com>
2017-12-26 13:06:29 +0100

    upgrade deps

--- webpack.config.dist.js
+++ webpack.config.dist.js
@@ -1,13 +1,15 @@
 const config = require('./webpack.config.default');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const path = require('path');
 
 module.exports = Object.assign({}, config, {
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      output: {
-        comments: false
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        output: {
+          comments: false
+        }
       }
     }),
     new CleanWebpackPlugin([ 'dist' ], {
